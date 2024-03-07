7e7ea3385680cb2051a050a87636035c5081faaa
Bastien <bureaubastien3@gmail.com>
2024-03-07 11:51:37 +0100

    Initial Commit

---
+++ Database/CRUDTest.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SocketBackend.Repository;
+using SocketBackend.Models;
+
+namespace GameOfLifeTests.Database
+{
+    [TestClass()]
+    public class CRUDTest
+    {
+        public UserRepository Repository { get; set; }
+
+        [TestInitialize]
+        public void Init()
+        {
+            Repository = new UserRepository(new SocketBackend.Context.GameOfLifeContext());
+        }
+
+        [TestMethod()]
+        public void AddUserInDB()
+        {
+            User user = new User() 
+            { 
+                Name = "Test",
+                Rule = "3A3M"
+            };
+
+            Repository.Insert(user);
+
+            Assert.AreEqual(3, Repository.GetAll().Count);
+        }
+
+        [TestMethod()]
+        public void UpdateTestDB()
+        {
+            var usr = Repository.FindByName("leo");
+            usr.Rule = "5A2M";
+
+            Repository.Update(usr);
+
+            Assert.AreEqual("5A2M", Repository.FindByName("leo").Rule);
+        }
+
+        [TestMethod()]
+        public void DeleteTestDB()
+        {
+            var usr = Repository.FindByID(2);
+            Repository.Delete(usr);
+
+            Assert.AreEqual(2, Repository.GetAll().Count);
+        }
+    }
+}
---
+++ Game/Logical/GameLogiqueTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GameOfLife.Game.Logical;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife.Game.Logical.Tests
+{
+    [TestClass()]
+    public class CRUDTest
+    {
+
+        private GameManager manager = null;
+
+        [TestInitialize] 
+        public void Init() 
+        {
+            manager = GameManager.Instance;
+            manager.Logical.Area[1, 0].IsAlive = true;
+            manager.Logical.Area[1, 1].IsAlive = false;
+            manager.Logical.Area[1, 2].IsAlive = true;
+            manager.Logical.Area[2, 1].IsAlive = true;
+            manager.Logical.Area[2, 2].IsAlive = true;
+            manager.Logical.Area[2, 3].IsAlive = true;
+        }
+
+        [TestMethod()]
+        public void TestGiveBirthToACell()
+        {
+            manager.Logical.SimulateGeneration();
+            Assert.IsTrue(manager.Logical.Area[1, 3].IsAlive, "La cellule 1,3 doit devenir vivante");
+        }
+
+        [TestMethod()]
+        public void TestKillACell()
+        {
+            manager.Logical.SimulateGeneration();
+            Assert.IsFalse(manager.Logical.Area[1, 0].IsAlive, "La cellule 1,0 doit être morte");
+        }
+    }
+}
